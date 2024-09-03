ceff94b105d3c8e77ddabea7bba8d456935c37bc
Huma-35042 <huma.work72@gmail.com>
2024-09-03 11:28:57 +0400

    Dark / Light mode Spinner added

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,11 +1,34 @@
-import loading from './loader.gif'
 import React from 'react'
+import { useContext, useState, useEffect } from 'react';
+import ThemeProvider, { ThemeModeContext } from '../context/ThemeContext'
+
+interface ThemeMode {
+  dataTheme: string;
+}
 
 
 export default function Spinner() {
+  const { dataTheme, setDataTheme } = useContext(ThemeModeContext);
+  const [loader, setLoader] = useState("");
+  
+  useEffect(() => {
+    changeLoader();
+
+  }, [dataTheme]);
+
+  const changeLoader = async () => {
+    if (dataTheme.dataTheme === "light") {
+      setLoader('../loader.gif');
+     }
+    else {
+      setLoader('../loader-dark.gif');
+     }
+
+  }
+
   return (
     <div className='text-center'> 
-    <img src={loading} alt='loading'></img>
+    <img src={loader} alt='loading'></img>
     
     
     </div>
